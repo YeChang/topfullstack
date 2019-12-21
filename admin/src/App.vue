fbb86dfe62bb56f5531697fde8883c4d3bf3a12a
changye66 <337894180@qq.com>
2019-12-21 10:22:12 +0800

    添加后台管理界面两个组件
    coursesList coursesEdit

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+@Component({
+  components: {}
+})
+export default class App extends Vue {}
+</script>  
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  margin: 0;
 }
 </style>
